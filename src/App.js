0374b66732e13ee57cc1e1193516e60ecb2595a1
Jogius <julius@themakowskis.de>
2020-09-17 16:41:28 +0200

    Navigation Bar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Routes from "./Routes";
+import Navigation from "./containers/Navigation";
 import "./App.css";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div className="header">
+          <Navigation />
+        </div>
         <div className="body">
           <Routes />
         </div>
---
+++ src/containers/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Navigation.css";
+
+export default class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="Navigation">
+        <Link to="" className="link">
+          Lorem Ipsum
+        </Link>
+      </div>
+    );
+  }
+}
